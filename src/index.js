8c29dfa7f1328d2bdca0d393922887718f03d0c0
Marcin Swaltek <swaltek@tutanota.com>
2022-05-22 08:08:37 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return <h1>Welcome to minimal_react_app!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
